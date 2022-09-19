924fede73615ac3416210f492bda3f2ab80dc97a
Thomas Json <joogletom8@gmail.com>
2022-09-19 11:58:37 +0200

    Publi maj

--- src/components/publication/Publication.jsx
+++ src/components/publication/Publication.jsx
@@ -3,27 +3,46 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import { RiUser3Line } from "react-icons/ri";
 import { AiOutlineMenu } from "react-icons/ai";
+import { BiMessageRounded, BiHeart, BiPaperPlane } from "react-icons/bi";
 
 const Publication = () => {
   return (
-    <Container fluid className="publication-bloc coL">
-      <Container fluid className="publication-header rOw betWeen align-center">
-        <Container fluid className="publication-creator align-center rOw">
-          <Container fluid className="profile-icon w-auto">
-            <RiUser3Line />
+    <>
+      <Container fluid className="publication-bloc coL">
+        <Container
+          fluid
+          className="publication-header rOw betWeen align-center"
+        >
+          <Container fluid className="publication-creator align-center rOw">
+            <Container fluid className="profile-icon w-auto">
+              <RiUser3Line />
+            </Container>
+            <Container fluid className="creator-name">
+              Profile Name
+            </Container>
           </Container>
-          <Container fluid className="creator-name">
-            Profile Name
+
+          <Container fluid className="publication-params">
+            <Container fluid>
+              <AiOutlineMenu />
+            </Container>
           </Container>
         </Container>
-
-        <Container fluid className="publication-params">
-          <Container fluid>
-            <AiOutlineMenu />
+        <Container fluid className="publication-content"></Container>
+        <Container fluid className="publication-react rOw">
+          <Container>
+            <BiHeart />
           </Container>
+          <Container>
+            <BiMessageRounded />
+          </Container>
+          <Container>
+            <BiPaperPlane />
+          </Container>
+
         </Container>
       </Container>
-    </Container>
+    </>
   );
 };
 
